bc3630d9bbb5fedcdaa2181c5b96e178779cde50
phm-tuyenn <phamthanhtuyen2k8@gmail.com>
2025-09-21 08:05:06 +0700

    update to ver 11 and done setup pedro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.configurables.annotations.Configurable;
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver.*;
+
+@Configurable
+public class RobotConstants {
+    public static final class PINPOINT {
+        public static final String hardwareName = "pinpoint";
+        public static final double xOffset = -84;
+        public static final double yOffset = 168;
+        public static final EncoderDirection forwardDirection = EncoderDirection.FORWARD;
+        public static final EncoderDirection strafeDirection = EncoderDirection.FORWARD;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drive/Constants.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.subsystems.drive;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.RobotConstants.*;
+
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(3.5);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rf")
+            .rightRearMotorName("rr")
+            .leftRearMotorName("lr")
+            .leftFrontMotorName("lf")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(PINPOINT.yOffset)
+            .strafePodX(PINPOINT.xOffset)
+            .distanceUnit(DistanceUnit.MM)
+            .hardwareMapName(PINPOINT.hardwareName)
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(PINPOINT.forwardDirection)
+            .strafeEncoderDirection(PINPOINT.strafeDirection);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
+                .build();
+    }
+}
